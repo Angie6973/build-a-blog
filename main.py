4fc84ad50f253552f826cadf86d29c9e121eb3a5
Angelika Stephanie Pierce <angiekinter@gmail.com>
2018-04-25 12:47:23 -0500

    created singlepost.html main part works on blog assignment, single posts not yet

--- main.py
+++ main.py
@@ -16,5 +16,56 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
+    def is_valid(self):
+        if self.title and self.body:
+            return True
+        else:
+            return False
+
+
+
+
+@app.route("/blog")
+def display_blog_entries():
+    
+    entry_id = request.args.get('id')
+    if (entry_id):
+        entry = Blog.query.get(entry_id)
+        return render_template('blog.html', entry=entry)
+
+    #
+    all_entries = Blog.query.all()
+    
+       
+    return render_template('blog.html', all_entries=all_entries)   
+    
+     
+
+
+@app.route("/newpost", methods=['POST', 'GET'])
+def new_entry():
+    if request.method =='POST':
+
+        blog_title=request.form['title'] 
+        blog_body=request.form['body'] 
+        new_entry=Blog(blog_title,blog_body) 
+
+        if new_entry.is_valid:
+
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect("/blog?id="+str(new_entry.id))        
+        
+
+        else:
+            flash("Please check your entry for errors. Both a title and body are required.")
+            return render_template("newpost.html")
+
+    else:
+        return render_template("newpost.html")
+
+    
+
+    
 if __name__ == '__main__':
     app.run()
